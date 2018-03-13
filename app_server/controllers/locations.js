415824a8dab6e732289431568cd2728add79464d
cyc1e <petroprom76@gmail.com>
2018-03-13 22:34:53 +0300

    homelistcontrollerDone

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,31 @@
 /* Получить (GET) домашнюю страницу */
 module.exports.homelist = function(req, res){
- res.render('locations-list', { title: 'Home' });
+ res.render('locations-list', { 
+ 	title: 'Loc8r - find a place to work with wifi',
+ 	pageHeader: {
+ 	  title: 'Loc8r',
+ 	  strapline: 'Find place to work with wifi near you!'
+ 	},
+ 	locations: [{
+ 		name: 'Bitch please',
+ 		address: 'Backer street 69',
+ 		rating: 3,
+ 		facilities: ['Hot drinks', 'Food', 'Premiun wifi'],
+ 		distance: '100m'
+ 	},{
+ 		name: 'No name',
+ 		address: 'Backer street 63',
+ 		rating: 4,
+ 		facilities: ['Hot drinks', 'Food', 'Premiun wifi'],
+ 		distance: '100m'
+ 	},{
+ 		name: 'Dumbs',
+ 		address: 'Backer street 3',
+ 		rating: 2,
+ 		facilities: ['Food', 'Premiun wifi'],
+ 		distance: '100m'
+ 	}]
+  });
 };
 /* Получить (GET) страницу с информацией о местоположениях */
 module.exports.locationInfo = function(req, res){
