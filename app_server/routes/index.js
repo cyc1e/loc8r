c01fc9ae07db5ecd04c0f7acc9eac0a05ad2c35d
cyc1e <petroprom76@gmail.com>
2018-03-13 13:51:22 +0300

    upgrade

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,5 +6,6 @@ var ctrlOthers = require('../controllers/others');
 router.get('/', ctrlLocattions.homelist);
 router.get('/location', ctrlLocattions.locationInfo);
 router.get('/location/review/new', ctrlLocattions.addReview);
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
