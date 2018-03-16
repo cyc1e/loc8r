76a1f796713092f977b22e9a8e9a7875991dd0ad
cyc1e <petroprom76@gmail.com>
2018-03-16 12:53:04 +0300

    connect to DB

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var ctrlLocattions = require('../controllers/locations');
+var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-router.get('/', ctrlLocattions.homelist);
-router.get('/location', ctrlLocattions.locationInfo);
-router.get('/location/review/new', ctrlLocattions.addReview);
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 router.get('/about', ctrlOthers.about);
 
 module.exports = router;
